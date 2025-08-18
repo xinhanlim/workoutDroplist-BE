5d4ab8dd806d95fa07c6276085f1ee0ff6d7b25d
xinhanlim <xhxhan00@gmail.com>
2025-08-18 07:38:05 +0000

    added database.js

---
+++ database/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const mongoURL = process.env.MONGO_URL;
+const dbName = process.env.MONGODB_DBNAME;
+
+
+async function connect(){
+    let client = await MongoClient.connect(mongoURL);
+    let db = client.db(dbName);
+    return db;
+}
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 let app = express();
-app.use(cors);
+app.use(cors());
 
+app.get('/', (req, res)=>{
+    res.json({
+        "message":"hello world"
+    })
+})
 
-
-app.listen(3000, () =>{
+app.listen(3200, () =>{
     console.log("Sever has started")
 })
